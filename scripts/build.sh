509f7d2bd8b581eaa558ef047f8051343393d6f9
Harry Chen <czy88840616@gmail.com>
2021-02-28 17:11:53 +0800

    chore: add api

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,3 +3,14 @@ set -e
 
 vuepress build docs
 node ./scripts/replace.js
+
+## 生成typedoc
+git clone git@github.com:midwayjs/midway.git
+cd midway
+npm i
+npm i typedoc@0.19.2 typedoc-neo-theme typedoc-plugin-lerna-packages
+npm run bootstrap
+npm run build
+npm run typedoc
+cd ..
+cp ./midway/docs/api-reference ./docs/.vuepress/dist
